346e8a480da78ffe519320e73e28caf830fe9569
abiskup <264346@student.pwr.edu.pl>
2023-04-01 23:26:23 +0200

    Initial commit

--- app/main.cpp
+++ app/main.cpp
@@ -7,29 +7,9 @@
 #endif
 
 #include <iostream>
-#include <stdlib.h>
+using namespace std;
 
-#include "exampleConfig.h"
-#include "example.h"
-
-/*
- * Simple main program that demontrates how access
- * CMake definitions (here the version number) from source code.
- */
 int main() {
-  std::cout << "C++ Boiler Plate v"
-            << PROJECT_VERSION_MAJOR
-            << "."
-            << PROJECT_VERSION_MINOR
-            << "."
-            << PROJECT_VERSION_PATCH
-            << "."
-            << PROJECT_VERSION_TWEAK
-            << std::endl;
-  std::system("cat ../LICENSE");
-
-  // Bring in the dummy class from the example source,
-  // just to show that it is accessible from main.cpp.
-  Dummy d = Dummy();
-  return d.doSomething() ? 0 : -1;
+  cout << "It's been a long time";
+  return 0;
 }
