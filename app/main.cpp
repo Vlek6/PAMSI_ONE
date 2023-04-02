e7e2f856e9259b27deee166051624141fe6f48b9
abiskup <264346@student.pwr.edu.pl>
2023-04-02 13:39:18 +0200

    Modify main for testing node.hpp

--- app/main.cpp
+++ app/main.cpp
@@ -4,12 +4,20 @@
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
+#include "node.hpp"
 #endif
 
 #include <iostream>
 using namespace std;
+#include "node.hpp"
 
 int main() {
   cout << "It's been a long time";
+  Node* exm = new Node();
+  Node* exm_val = new Node("sialala");
+
+  cout << exm->data << endl << exm_val->data << endl;
+
+
   return 0;
 }
